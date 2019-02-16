568e54fec6384b130c46a0d8be98efe32026bdd6
mcmurr32 <spencermcmurray@yahoo.ca>
2019-02-15 22:03:08 -0500

    Created POST request to second html file

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        # Run Script
+        brain = "Yeet"
+        tumor = "Yaah"
+        return render_template('scansDisplay.html', brain=brain, tumor=tumor)
     return render_template('index.html')
 
 
